e5e21d121789aaf49cc71db6f19f5f14fd6f1a99
mardld <m.dailidonis@gmail.com>
2020-03-09 23:21:04 +0200

    initial commit

---
+++ src/main/kotlin/mardld/sitecrawler/SiteCrawlerApplication.kt
@@ -0,0 +1,11 @@
+package mardld.sitecrawler
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SiteCrawlerApplication
+
+fun main(args: Array<String>) {
+	runApplication<SiteCrawlerApplication>(*args)
+}
---
+++ src/test/kotlin/mardld/sitecrawler/SiteCrawlerApplicationTests.kt
@@ -0,0 +1,13 @@
+package mardld.sitecrawler
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SiteCrawlerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
